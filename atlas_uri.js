c4de2b65090cd99b9d8b351b1f518a3d00eb006b
Shanthini Parthiban <125378706+shanthini200@users.noreply.github.com>
2025-08-05 20:36:18 +0530

    Update atlas_uri.js

--- atlas_uri.js
+++ atlas_uri.js
@@ -1,9 +1,3 @@
-// module.exports = uri = mongodb+srv:shanthiniparthi28122:Jv7dy2yJjMwqmDkq@cluster0.zwmkwhf.mongodb.net/Connection?retryWrites=true&w=majority&appName=Cluster0;
-
-// const uri = mongodb+srv:shanthiniparthi28122:P6yO7oYfeDmU3j7x@cluster0.wrl5hrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
-
-// module.exports = uri;
-
 
 require('dotenv').config(); // Load environment variables
 
